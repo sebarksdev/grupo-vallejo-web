6689817d33c057e7c536a65a799a323b1cac4695
sebarksdev <sebasvs96@gmail.com>
2021-10-22 18:11:08 -0600

    Se agregaron servicios de vauto

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,33 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+const targetDiv = document.getElementById("first");
+const btn = document.getElementById("toggle");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
+
+const targetDiv2 = document.getElementById("second");
+const btn2 = document.getElementById("toggle2");
+btn2.onclick = function () {
+  if (targetDiv2.style.display !== "none") {
+    targetDiv2.style.display = "none";
+  } else {
+    targetDiv2.style.display = "block";
+  }
+};
+
+const targetDiv3 = document.getElementById("third");
+const btn3 = document.getElementById("toggle3");
+btn3.onclick = function () {
+  if (targetDiv3.style.display !== "none") {
+    targetDiv3.style.display = "none";
+  } else {
+    targetDiv3.style.display = "block";
+  }
+};
